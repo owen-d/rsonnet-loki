2ad3a4130ffa73ccd218402734797c1dc6d1c565
Owen Diehl <ow.diehl@gmail.com>
2022-01-23 20:09:14 -0500

    extend with trait with optional methods Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- src/paras/conventions/with.rs
+++ src/paras/conventions/with.rs
@@ -4,8 +4,26 @@ pub trait With<T>
 where
     Self: Clone + Has<T>,
 {
+    /// with will put a <T> in the relevant place(s).
     #[must_use]
     fn with(&self, x: T) -> Self;
+
+    /// with_fn will apply a mapping function to the <T>s in relevant places,
+    /// but may skip Option<T>.
+    #[must_use]
+    fn with_fn(&self, f: fn(T) -> T) -> Self {
+        if let Some(x) = self.get().map(f) {
+            return self.with(x);
+        }
+        self.clone()
+    }
+
+    /// with_default will put a mapped <T> into the relevant place(s),
+    /// applying the function to a default value if the actual entry was missing.
+    #[must_use]
+    fn with_default(&self, f: fn(T) -> T, def: T) -> Self {
+        self.with(f(self.get().unwrap_or(def)))
+    }
 }
 
 impl<T> With<T> for T
@@ -23,5 +41,18 @@ where
 {
     fn with(&self, x: T) -> Self {
         Some(x)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_with_default_impl() {
+        let x: Option<i32> = None;
+        assert_eq!(Some(1), x.with(1));
+        assert_eq!(None, x.with_fn(|x: i32| x + 1));
+        assert_eq!(Some(2), x.with_default(|x| x + 1, 1));
     }
 }
